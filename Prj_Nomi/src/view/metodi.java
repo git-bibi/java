fb903ed79dd4aeb5e30bad6caacb033d5a6e1a15
Ludovico Cipolletta <ICTS21-23.166@studenti.local>
2022-04-12 15:05:21 +0200

    Ho cambiato il syso del progetto

--- Prj_Nomi/src/view/metodi.java
+++ Prj_Nomi/src/view/metodi.java
@@ -1,9 +1,11 @@
 package view;
 
+
 public class metodi {
+	public static void Nome3(){System.out.println("Ciao sono Ludovico")};
 
 	public static void main(String[] args) {
-		System.out.println("Ciao sono Ludovico");
+
 		Nome1();
 		Nome2();
 		Nome3();
